2bb9db2fb4ee70616d24b0fd439d2b2be6ee552e
rodericasm <rodericasm@gmail.com>
2020-10-31 09:08:17 +1000

    test travis

--- src/components/header.js
+++ src/components/header.js
@@ -51,10 +51,7 @@ const Header = ({book_title, dll_txt}) => {
     <path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
     <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
   </svg></Link> 
-
-
   </div>
-
   <h1>{book_title}</h1>
   
   <div className="nav_group">
